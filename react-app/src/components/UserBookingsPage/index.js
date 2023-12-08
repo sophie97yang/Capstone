c0d170156fa2b144fdcbe843ea1f2ca63bfef707
Sophie Yang <sophie97yang@gmail.com>
2023-12-08 07:42:16 -0800

    landing page stylings and added footer

--- react-app/src/components/UserBookingsPage/index.js
+++ react-app/src/components/UserBookingsPage/index.js
@@ -1,6 +1,10 @@
+import './UserBookings.css';
+import {Link} from 'react-router-dom';
+
 function UserBookings() {
     return (
-        <div>
+        <div className='user-bookings'>
+             <h3 className='breadcrumb-container'><Link to='/' className='breadcrumb'>Home </Link> {`  >`} Your Bookings</h3>
             <h2>Your Bookings</h2>
             <p>Feature coming soon. Please try again later.</p>
         </div>
