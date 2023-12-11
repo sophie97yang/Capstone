7e82d674cfcfa79c7929bff12eea8a5a7ff11c46
Sophie Yang <sophie97yang@gmail.com>
2023-12-11 07:19:54 -0800

    error handling for images

--- react-app/src/components/AllTripsPage/index.js
+++ react-app/src/components/AllTripsPage/index.js
@@ -73,7 +73,9 @@ const AllTrips = () => {
            { trips.length ? trips.map(trip=> (
             <div key={trip.id} className='individual-trip'>
             <div className='my_trips-left'>
-            <img src={trip.trip.image?trip.trip.image:images[choice]} alt={trip.trip.name}></img>
+            <img src={trip.trip.image?trip.trip.image:images[choice]} alt={trip.trip.name}
+            onError={e => { e.currentTarget.src = "https://images.unsplash.com/photo-1522878129833-838a904a0e9e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+            ></img>
             {trip.until>=0 ? <p className='countdown'>In {trip.until} days</p>:<p className='countdown'>Past Trip</p>}
             </div>
             <div className='my_trips-right'>
