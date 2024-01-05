4a57b201bb18e0d0f523dc3ce6158d2ef3e47760
Sophie Yang <sophie97yang@gmail.com>
2024-01-05 11:43:30 -0800

    removed console log

--- react-app/src/components/TripDetailsPage/Itinerary.js
+++ react-app/src/components/TripDetailsPage/Itinerary.js
@@ -8,7 +8,6 @@ const Itinerary = ({trip}) => {
     const refs = useRef([]);
     const [visible,setVisible] = useState(0);
     const currUser=useSelector(state => state.session.user);
-    console.log(currUser);
     //get a list of dates in between trip start and end date
     const daysOfWeek = {0:"Sunday",1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday"};
     const months = {0:"January",1:"February",2:"March",3:"April",4:"May",5:"June",6:"July",7:"August",8:"September",9:"October",10:"November",11:"December"}
